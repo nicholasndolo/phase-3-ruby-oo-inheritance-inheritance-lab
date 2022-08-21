7c8b3d65d25701e0686bd07f507fe2e26a498c68
Nicholas <nicholas.nmalinda@gmail.com>
2022-08-21 14:09:11 +0300

    Lab completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(words)
+        @knowledge << words
+    end
+    
+    def knowledge
+        @knowledge
+    end
 
 end
